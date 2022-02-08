0e6d5e099a76d92f7098c5cc97de687017e68f63
carlos roberto <carlosrobertounopar01@hotmail.com>
2022-02-08 20:34:04 -0300

    Calculando a frequência de cada tipo de pedido

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,19 +1,38 @@
 defmodule ReportsGenerator do
+  @availabel_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
   def build(filename) do
     alias ReportsGenerator.Parser
 
     filename
     |> Parser.parse_file()
-    |> Enum.reduce(report_acc(), fn line, acc -> sum_values(line, acc) end)
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
   def fetch_higher_cost(report) do
     Enum.max_by(report, fn {_key, value} -> value end)
   end
 
-  defp sum_values([id, _food_name, price], acc) do
-    Map.put(acc, id, acc[id] + price)
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    %{report | "users" => users, "foods" => foods}
   end
 
-  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+  defp report_acc do
+    foods = Enum.into(@availabel_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{"users" => users, "foods" => foods}
+  end
 end
