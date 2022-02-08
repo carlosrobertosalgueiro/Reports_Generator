ff986c65cd31f4dc01f3fd54dd9e04b3badc4fa0
carlos roberto <carlosrobertounopar01@hotmail.com>
2022-02-07 22:46:55 -0300

    somando valores por id

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -2,9 +2,9 @@ defmodule ReportsGenerator do
   def build(filename) do
     "reports/#{filename}"
     |> File.stream!()
-    |> Enum.reduce(%{}, fn line, acc ->
+    |> Enum.reduce(report_acc(), fn line, acc ->
       [id, _food_name, price] = parse_line(line)
-      Map.put(acc, id, price)
+      Map.put(acc, id, acc[id] + price)
     end)
   end
 
@@ -14,4 +14,6 @@ defmodule ReportsGenerator do
     |> String.split(",")
     |> List.update_at(2, &String.to_integer/1)
   end
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
