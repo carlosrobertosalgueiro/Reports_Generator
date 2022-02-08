5ea0bbea78c1042af33d1455f178d935a611bc95
carlos roberto <carlosrobertounopar01@hotmail.com>
2022-02-08 07:20:58 -0300

    refatorado

---
+++ lib/parser.ex
@@ -0,0 +1,16 @@
+defmodule ReportsGenerator.Parser do
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(fn line -> parse_line(line) end)
+
+    # |> Enum.each(fn elem -> IO.inspect(elem) end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+  end
+end
