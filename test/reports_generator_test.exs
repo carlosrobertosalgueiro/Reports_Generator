40b2f3076c587f3e43cc8ff7418be6a5ca7bd8b1
carlos roberto <carlosrobertounopar01@hotmail.com>
2022-02-12 18:33:41 -0300

    Testando o ReportsGenerator

--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -58,4 +58,45 @@ defmodule ReportsGeneratorTest do
       assert response == expected_response
     end
   end
+
+  describe "fetch_higher_cost/2" do
+    test "when the option is 'users', returns the user who spent the most " do
+      file_name = "report_test.csv"
+
+      response =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("users")
+
+      expected_response = {:ok, {"5", 49}}
+
+      assert response == expected_response
+    end
+
+    test "when the option is 'users', returns the most comsumed food" do
+      file_name = "report_test.csv"
+
+      response =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("foods")
+
+      expected_response = {:ok, {"esfirra", 3}}
+
+      assert response == expected_response
+    end
+
+    test "when an ivalid option is given, returns an error" do
+      file_name = "report_test.csv"
+
+      response =
+        file_name
+        |> ReportsGenerator.build()
+        |> ReportsGenerator.fetch_higher_cost("fo")
+
+      expected_response = {:error, "Invalid Option!"}
+
+      assert response == expected_response
+    end
+  end
 end
